3c616e373cded4e4fd3b683b1aaa2e279310cd70
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-12-24 14:30:17 +0500

    test facade implementation

--- FacadePattern/Program.cs
+++ FacadePattern/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FacadePattern.Models;
+using System;
 
 namespace FacadePattern
 {
@@ -10,6 +7,12 @@ namespace FacadePattern
     {
         static void Main(string[] args)
         {
+            //Check feature was activate by tutor in the system
+            CheckAction action = new CheckAction(new Tutor(1111));
+            action.Check();
+
+
+            Console.ReadKey();
         }
     }
 }
