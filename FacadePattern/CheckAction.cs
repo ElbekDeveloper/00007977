6a31c1d3a1f46f4a6da95748d292856b3600f31b
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-12-24 14:29:47 +0500

    Add facade

---
+++ FacadePattern/CheckAction.cs
@@ -0,0 +1,31 @@
+﻿using FacadePattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    public class CheckAction
+    {
+        private Tutor _tutor;
+
+        public CheckAction(Tutor tutor)
+        {
+            _tutor = tutor;
+        }
+
+        public void Check()
+        {
+            //Tutor is in the student profile within the system
+            Console.WriteLine("Getting student details...");
+            Student student = new Student(7777);
+            Console.WriteLine("Getting the coursework file...");
+            Coursework coursework = new Coursework(_tutor.InstructingModuleCode, student);
+            Console.WriteLine("Opening the file...");
+            _tutor.Check(coursework);
+            Console.WriteLine("Checking is complete...");
+        }
+    }
+}
