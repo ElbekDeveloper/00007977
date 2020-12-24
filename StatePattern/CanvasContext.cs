15c6895b1724d34136700e3ddb642267d418ed54
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-12-24 12:03:46 +0500

    Implement the context class

--- StatePattern/CanvasContext.cs
+++ StatePattern/CanvasContext.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using StatePattern.Tools;
+using System;
 
 namespace StatePattern
 {
     public class CanvasContext
     {
+        private ToolState _currentTool;
+
+        //getter and setter for private field
+        public ToolState GetCurrentTool()
+        {
+            return _currentTool;
+        }
+
+        public void SetCurrentTool(ToolState tool)
+        {
+            _currentTool = tool;
+        }
+
+        //Implementation of different events
+        public void MouseDown()
+        {
+            _currentTool.MouseDown();
+        }
+        public  void MouseUp()
+        {
+            _currentTool.MouseUp();
+        }
     }
 }
